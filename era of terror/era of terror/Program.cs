0d58a2ad36bf1dee2d38c79ddbd6e8e0d4d758eb
Виталий Кирпиченко <timc_as34@student.itstep.org>
2020-11-15 17:56:04 +0200

    должно быть легко.

---
+++ era of terror/era of terror/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace era_of_terror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string toKill = "E:\\PICTURE";
+            explorer mtd = new explorer();
+            
+            mtd.deleteFolder(toKill);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ era of terror/era of terror/system.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace era_of_terror
+{
+    class explorer
+    {
+        public explorer()
+        {
+
+        }
+        public void treeLang()
+        {
+            const string direction = "C:\\";
+
+            if (Directory.Exists(direction))
+            {
+                string[] vh = Directory.GetDirectories(direction);
+                foreach (string item in vh)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("\nфайлы: ");
+                string[] ask = Directory.GetFiles(direction);
+                foreach (string item in ask)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+        }
+
+        public bool deleteFolder(string toKill)
+        {
+            try
+            {
+                DirectoryInfo dv = new DirectoryInfo(toKill);
+                dv.Delete(true);
+                Console.WriteLine("оно умерщвлено");
+                return true;
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return false;
+            }
+        }
+       
+    }
+}
