d40351b094d1b96b66912d403396ea701a2e5154
Виталий Кирпиченко <timc_as34@student.itstep.org>
2020-11-30 18:54:57 +0200

    осталось копирование и перемещение.

--- era of terror/era of terror/Program.cs
+++ era of terror/era of terror/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace era_of_terror
 {
@@ -11,26 +14,89 @@ namespace era_of_terror
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("instructions:\n\ne- switching drives\nr- rename\ndel- delete something\nenter- move on\nnavigate with arrows\n");
+            Console.ReadKey();
+
             #region settings
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Red;
             #endregion
+
+            
             explorer ex = new explorer();
 
-            DirectoryInfo mtd = new DirectoryInfo("C:\\");
-            ex.shortenedRoutes = explorer.fetchDirectories(mtd, ex.fullRoutes);
+            DirectoryInfo mtd = new DirectoryInfo(explorer.AllDrives[0].Name);
+            List<string> antique = new List<string> { };
+            int returnCount = 1;
+
             
+
             while(true)
             {
-                Console.Clear();
-                int demand = graphicMenu.VerticalMenu(ex.prepareTheTree(ex.shortenedRoutes));
+                antique.Add(explorer.currentLocation);
+
+                ex.shortenedFileRoutes = explorer.haulFiles(mtd);
+                ex.shortenedRoutes = explorer.fetchDirectories(mtd);
+                ex.fullRoutes = explorer.convertStrings(ex.shortenedRoutes);
+                ex.fullFileRoutes = explorer.convertLines(ex.shortenedFileRoutes);
 
                 Console.Clear();
+                Console.WriteLine(explorer.currentLocation + "\n");
 
-                DirectoryInfo mt = ex.fullRoutes[demand];
-                ex.shortenedRoutes = explorer.fetchDirectories(mt, ex.fullRoutes);//тут ошибка где- то
+                List<string> rits = new List<string> { };
+                rits.AddRange(ex.prepareTheTree(ex.shortenedRoutes));
+                rits.AddRange(ex.prepareTheTree(ex.shortenedFileRoutes));
+                string[] kats = rits.ToArray();
 
-                //graphicMenu.VerticalMenu(ex.prepareTheTree(ex.shortenedRoutes));
+                var demand = graphicMenu.VerticalMenu(kats, ex);
+
+                Console.Clear();
+
+                if (demand == -13)
+                {
+                    
+                }
+                else if (demand == -9)
+                {
+                    List<string> unl = new List<string> { };
+                    List<string> vh = new List<string> { };
+
+                    foreach (DriveInfo item in explorer.AllDrives)
+                    {
+                        unl.Add(item.Name);
+                        vh.Add(item.Name + $"\t{item.DriveFormat}" + $"\t{item.TotalFreeSpace}bytes from {item.TotalSize}bytes");
+                    }
+
+                    int spray = graphicMenu.VerticalMenu(vh.ToArray());
+
+                    DirectoryInfo temple = new DirectoryInfo(unl[spray]);
+                    mtd = temple;
+                }
+                else if (demand != -2)
+                {
+                    returnCount = 1;
+                    try
+                    {
+                        mtd = ex.fullRoutes[demand];
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        try
+                        {
+                            Process.Start(ex.shortenedFileRoutes[demand - ex.shortenedRoutes.Length]);
+                        }
+                        catch (Exception ppun)
+                        {
+                            MessageBox.Show($"{ppun.Message}", "an exception has occured");
+                        }
+                    }
+                }
+                else
+                {
+                    DirectoryInfo temporary = new DirectoryInfo(antique[antique.Count - returnCount]);
+                    mtd = temporary;
+                    returnCount += 2;
+                }
             }
         }
     }
--- era of terror/era of terror/system.cs
+++ era of terror/era of terror/system.cs
@@ -4,36 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
 
 namespace era_of_terror
 {
     class explorer
-    {
-        private static string currentLocation;
+    { 
+        public int moveNum = -1;
+        public string moveString { get; set; }
+        public static List<DriveInfo> AllDrives = DriveInfo.GetDrives().ToList();
+
+        public static string currentLocation = "";
         public string[] shortenedRoutes { get; set; }
         public List<DirectoryInfo> fullRoutes { get; set; }
+
+        public string[] shortenedFileRoutes { get; set; }
+        public List<FileInfo> fullFileRoutes { get; set; }
+
+        //public List<>
         public explorer()
         {
-            currentLocation = "";
+            moveString = "";
             shortenedRoutes = new string[] { };
             fullRoutes = new List<DirectoryInfo> { };
+
+            shortenedFileRoutes = new string[] { };
+            fullFileRoutes = new List<FileInfo> { };
         }
-        public static string[] fetchDirectories(DirectoryInfo mtd, List<DirectoryInfo> storage)
+
+
+
+        public static string[] fetchDirectories(DirectoryInfo mtd)
         {
             currentLocation = mtd.FullName;
 
-            string[] ask = Directory.GetDirectories(mtd.FullName);
-            foreach(string item in ask)
+            string[] ask = new string[] { };
+            try
+            {
+                ask = Directory.GetDirectories(mtd.FullName);
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            return ask;
+        }
+
+        public static string[] haulFiles(DirectoryInfo mtd)
+        {
+            string[] ayanami = new string[] { };
+            try
+            {
+                ayanami = Directory.GetFiles(mtd.FullName);
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            return ayanami;
+        }
+
+        public static List<DirectoryInfo> convertStrings(string[] roots)
+        {
+            List<DirectoryInfo> storage = new List<DirectoryInfo> { };
+
+            foreach (string item in roots)
             {
                 DirectoryInfo temp = new DirectoryInfo(item);
                 storage.Add(temp);
             }
-            return ask;
+
+            return storage;
         }
+
+        public static List<FileInfo> convertLines(string[] fileRoots)
+        {
+            List<FileInfo> storage = new List<FileInfo> { };
+
+            foreach (string item in fileRoots)
+            {
+                FileInfo temp = new FileInfo(item);
+                storage.Add(temp);
+            }
+
+            return storage;
+        }
+
         public string[] prepareTheTree(string[] ask)
         {
             List<string> vh = new List<string> { };
-            foreach(string item in ask)
+            foreach (string item in ask)
             {
                 char mz = '\\';
                 int sl = 0;
@@ -50,9 +110,10 @@ namespace era_of_terror
                 }
             }
 
-            string[]langley = vh.ToArray();
+            string[] langley = vh.ToArray();
             return langley;
         }
+
 
         public bool deleteFolder(string toKill)
         {
@@ -63,20 +124,112 @@ namespace era_of_terror
                 Console.WriteLine("оно умерщвлено");
                 return true;
             }
-            catch(Exception exc)
+            catch (Exception exc)
             {
                 Console.WriteLine(exc.Message);
                 return false;
             }
         }
-       
-    }
+
+        public bool rename(DirectoryInfo mt, string sati)
+        {
+            try
+            {
+                Directory.Move(mt.FullName, sati);
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "an exception has occured");
+                return false;
+            }
+        }
+        public bool rename(FileInfo mt, string sati)
+        {
+            try
+            {
+                File.Move(mt.FullName, sati);
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "an exception has occured");
+                return false;
+            }
+
+        }
+
+        }
+        #region gololobov
+        class graphicMenu
+        {
+            public static int VerticalMenu(string[] elements)
+            {
+                int maxLen = 0;
+                foreach (var item in elements)
+                {
+                    if (item.Length > maxLen)
+                        maxLen = item.Length;
+                }
+                ConsoleColor bg = Console.BackgroundColor;
+                ConsoleColor fg = Console.ForegroundColor;
+                int x = Console.CursorLeft;
+                int y = Console.CursorTop;
+                Console.CursorVisible = false;
+                int pos = 0;
+                while (true)
+                {
+
+                    for (int i = 0; i < elements.Length; i++)
+                    {
+                        Console.SetCursorPosition(x, y + i);
+                        if (i == pos)
+                        {
+                            Console.BackgroundColor = fg;
+                            Console.ForegroundColor = bg;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = bg;
+                            Console.ForegroundColor = fg;
+                        }
+                        Console.Write(elements[i].PadRight(maxLen));
+                    }
+
+                    ConsoleKey consoleKey = Console.ReadKey().Key;
+                    switch (consoleKey)
+                    {
+
+                        case ConsoleKey.Enter:
+                            return pos;
+                            break;
+
+                        case ConsoleKey.Escape:
+                            return -2;
+                            break;
+
+                        case ConsoleKey.E:
+                            return -9;
+                            break;
+
+                        case ConsoleKey.UpArrow:
+                            if (pos > 0)
+                                pos--;
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            if (pos < elements.Length - 1)
+                                pos++;
+                            break;
 
 
-    #region gololobov
-    class graphicMenu
-    {
-        public static int VerticalMenu(string[] elements)
+                        default:
+                            break;
+                    }
+
+                }
+            }
+        public static int VerticalMenu(string[] elements, explorer ex)
         {
             int maxLen = 0;
             foreach (var item in elements)
@@ -114,14 +267,55 @@ namespace era_of_terror
                 {
 
                     case ConsoleKey.Enter:
+                        return pos;
+                        break;
+
+                    case ConsoleKey.Escape:
+                        return -2;
+                        break;
+
+                    case ConsoleKey.E:
+                        return -9;
+                        break;
+
+                    case ConsoleKey.R:
                         {
+                            Console.Clear();
+                            try
+                            {
+                                Console.WriteLine(explorer.currentLocation + $"\n{ex.shortenedRoutes[pos]} ->");
+                                string newName = explorer.currentLocation + "\\" + Console.ReadLine();
+                                ex.rename(ex.fullRoutes[pos], newName);
+                            }
+                            catch(IndexOutOfRangeException)
+                            {
+                                Console.WriteLine(explorer.currentLocation + $"\n{ex.shortenedFileRoutes[pos - ex.fullRoutes.Count]} -> ");
+                                string newName = explorer.currentLocation + "\\" + Console.ReadLine();
+                                ex.rename(ex.fullFileRoutes[pos - ex.fullRoutes.Count], newName);
+                            }
+
+                            return -13;
+                        }
+                        break;
+
+                    case ConsoleKey.C:
+                        {
+                            ex.moveNum = pos;
                             return pos;
                         }
                         break;
 
-                    case ConsoleKey.Escape:
-                        return elements.Length - 1;
+                    case ConsoleKey.X:
+                        {
+                            ex.moveNum = pos;
+                            return pos;
+                        }
                         break;
+
+                    case ConsoleKey.V:
+                        {
+
+                        }
 
                     case ConsoleKey.UpArrow:
                         if (pos > 0)
@@ -133,6 +327,7 @@ namespace era_of_terror
                             pos++;
                         break;
 
+
                     default:
                         break;
                 }
@@ -141,9 +336,9 @@ namespace era_of_terror
         }
 
         public static int HorizontalMenu(string[] elements)
-        {
-            return 0;
+            {
+                return 0;
+            }
         }
-    }
-    #endregion
+        #endregion
 }
